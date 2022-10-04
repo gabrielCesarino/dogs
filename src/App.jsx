910b88bfd69c101b49eceeb960abc2675581a52d
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-10-04 07:23:41 -0300

    feat: initial router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Footer } from './Components/Footer'
+import { Header } from './Components/Header'
+import { Home } from './Components/Home'
+
 function App() {
 
   return (
-    <h1>
-     Hello 
-    </h1>
+    <>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+      </BrowserRouter>
+    </>
   )
 }
 
